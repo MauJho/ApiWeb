ac02b89972b89aeb5685450df6965cd323dffddd
MauJho <maurieljng@gmail.com>
2022-02-08 10:32:46 -0600

    JWT and refersh token

---
+++ Api Web/Models/RefreshToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_Web.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Token { get; set; }
+        public string JwtId { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateExpire { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+    }
+}
